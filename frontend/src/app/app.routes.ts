ab54491c07eb39856946bf1337fe5edc01e90692
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-07-10 14:46:17 -0300

    feat: navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'card', component: CardComponent}
 ];
