3b937bd90222aafcd415460415104a3ae09f8a06
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-07-10 15:48:06 -0300

    feat: movie card e home items

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
