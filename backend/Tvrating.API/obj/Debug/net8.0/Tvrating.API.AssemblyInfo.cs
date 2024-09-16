0f4eff22b81414c579847e538887cc665c9cd5ea
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-09-16 17:21:07 -0300

    Add arquitetura limpa com asp.net

--- backend/Tvrating.API/obj/Debug/net8.0/Tvrating.API.AssemblyInfo.cs
+++ backend/Tvrating.API/obj/Debug/net8.0/Tvrating.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tvrating.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+518f2ae4f6ff4f2e9416dbe1b3913c266faa1057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd59e0ede4359db3f6e12b19ee6226ca732af7ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tvrating.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tvrating.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/src/main/java/com/gabriel/backend/repository/ShowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gabriel.backend.repository;
-
-import com.gabriel.backend.domain.Show;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShowRepository extends JpaRepository<Show, Long> {
-}
