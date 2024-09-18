9ff466038d45ecb4d42c41276839000603962b98
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-09-18 14:36:13 -0300

    add obj files to gitignore

---
+++ backend/Tvrating.API/MovieMeter.Infrastructure/Persistence/Migrations/20240918145843_user entity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieMeter.Infrastructure.Persistence
+{
+    /// <inheritdoc />
+    public partial class userentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ backend/Tvrating.API/MovieMeter.Infrastructure/Persistence/Migrations/20240918152340_update user e show entity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieMeter.Infrastructure.Persistence
+{
+    /// <inheritdoc />
+    public partial class updateusereshowentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Shows",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shows_UserId",
+                table: "Shows",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shows_Users_UserId",
+                table: "Shows",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shows_Users_UserId",
+                table: "Shows");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shows_UserId",
+                table: "Shows");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Shows");
+        }
+    }
+}
