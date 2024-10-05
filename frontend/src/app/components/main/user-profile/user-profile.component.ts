18509479074b5af25a8edaca6851c1f97f9ae035
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2024-10-05 17:45:08 -0300

    página de visualização do perfil do usuário

---
+++ frontend/src/app/components/main/user-profile/user-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+}
